7660edc429ed8765fdba1da9dcc5c26984c178a6
Huashen87 <60507281+Huashen87@users.noreply.github.com>
2021-03-23 01:29:57 +0800

    Separate Deck from GoodGame

--- src/SRP/Good.ts
+++ src/SRP/Good.ts
@@ -1,40 +1,21 @@
-import { Card } from './Card';
+import { Deck } from './Deck';
 
 export class GoodGame {
-  private deck: Card[] = [];
+  private deck: Deck;
+  private handcards: Deck;
   constructor() {
-    this.initialDeck(this.deck);
-    this.sheffleDeck(this.deck);
-    this.startGame();
-  }
+    this.deck = new Deck();
+    console.log('Initial Deck!');
+    this.deck.print();
 
-  private startGame(): void {
+    this.deck.sheffle();
+    console.log('Sheffle Deck!');
+    this.deck.print();
+
     console.log('Game Start!');
-    const handcards = this.getHandcards(this.deck);
-    this.printCards(handcards);
-  }
 
-  private initialDeck(deck: Card[]): void {
-    for (let i = 0; i < 52; i++) deck[i] = new Card(i + 1);
-  }
-
-  private sheffleDeck(deck: Card[]): void {
-    for (let i = deck.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
-  }
-
-  private getHandcards(deck: Card[]): Card[] {
-    const handcards: Card[] = [];
-    let i = 0;
-    deck.forEach((card) => {
-      if (!(i++ % 4)) handcards.push(card);
-    });
-    return handcards;
-  }
-
-  private printCards(cards: Card[]): void {
-    cards.forEach((card) => console.log(card.toString()));
+    this.handcards = this.deck.split(13);
+    console.log('handcard');
+    this.handcards.print();
   }
 }
