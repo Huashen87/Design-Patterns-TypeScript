332356d7868469118eb9d315f019ca25940ba0c3
Huashen87 <60507281+Huashen87@users.noreply.github.com>
2021-04-10 00:15:54 +0800

    Add toValue function and change the suit

--- src/OCP/Card.ts
+++ src/OCP/Card.ts
@@ -1,22 +1,26 @@
 export class Card {
   constructor(private value: number) {}
-  toString(): string {
+  public toString(): string {
     let num: number = Math.ceil(this.value / 4);
     let suit: string = '';
     switch (this.value % 4) {
       case 0:
-        suit = '\u2660';
+        suit = '\u2663';
         break;
       case 1:
-        suit = '\u2663';
+        suit = '\u2666';
         break;
       case 2:
         suit = '\u2665';
         break;
       case 3:
-        suit = '\u2666';
+        suit = '\u2660';
         break;
     }
     return `[${suit} ${num}]`;
+  }
+
+  public toValue(): number {
+    return this.value;
   }
 }
