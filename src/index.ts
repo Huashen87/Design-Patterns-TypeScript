2e74a294db59ad565e3aa184bfa5aefa53c0c943
Huashen87 <60507281+Huashen87@users.noreply.github.com>
2021-04-10 00:15:08 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // import { BadGame } from './SRP/Bad';
 // new BadGame();
 
-import { GoodGame } from './SRP/Good';
+// import { GoodGame } from './SRP/Good';
+// new GoodGame();
+
+import { GoodGame } from './OCP/Good';
 new GoodGame();
